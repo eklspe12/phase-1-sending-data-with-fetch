1c886ee4c0be246ec71ab5b93cd6a2f93807433a
Spencer <eklund.spencer@outlook.com>
2023-08-04 15:53:46 -0600

    finally done

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-// Add your code here
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
+
+function submitData(name, email) {
+  const user = {
+    name: name,
+    email: email,
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => res.json())
+    .then((newUser) => {
+      console.log(newUser);
+      appendUser(newUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      appendError(err.message);
+    });
+}
+
+function appendUser(newUser) {
+  const b = document.querySelector("body");
+  const addUser = document.createElement("div");
+  addUser.innerHTML = `<h1>ID: ${newUser.id}</h1>
+      <h1>Name ${newUser.name}</h1>
+      <h1>Email ${newUser.email}</h1>`;
+
+  addUser.setAttribute("id", `user-${newUser.id}`);
+
+  b.appendChild(addUser);
+}
+
+function appendError(errorMessage) {
+  const b = document.querySelector("body");
+  const errorP = document.createElement("p");
+  errorP.innerHTML = errorMessage;
+  b.appendChild(errorP);
+}
+
+submitData("Spencer", "email.com");
